b4a9bb071c6df55117790a231589da2ed3d617e4
ArunmathavanK <133943485+ArunmathavanK@users.noreply.github.com>
2024-03-15 20:34:00 +0530

    Add files via upload

---
+++ To find transition point.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int transitionPoint(int arr[], int n) {
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            return i;
+    }
+}
+return -1;
+}
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.transitionPoint(a, n) << endl;
+    }
+    return 0;
+}
+
