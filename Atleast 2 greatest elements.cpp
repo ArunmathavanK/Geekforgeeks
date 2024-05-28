660ce585f5c3021a1ac1f4db2cd6270074ab2a67
ArunmathavanK <133943485+ArunmathavanK@users.noreply.github.com>
2024-05-28 12:08:17 +0530

    Add files via upload

---
+++ Atleast 2 greatest elements.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    vector<int> findElements(int a[], int n)
+    {
+        sort(a,a+n);
+        vector<int>ans;
+        for(int i=0;i<n-2;i++){
+            ans.push_back(a[i]);
+        }
+        return ans;
+    }
+};
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        Solution ob;
+        vector <int> res = ob.findElements(a,n);
+        for(int i=0;i<res.size();i++)
+        cout<<res[i]<<" ";
+        cout<<endl;
+    }
+}
